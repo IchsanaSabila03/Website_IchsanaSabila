e1ba4bb2b4dfd0b64af2c370199e6e9561c08a23
ichsanasabila03 <ichsanasabila003@gmail.com>
2022-06-27 20:07:59 +0700

    project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
 @extends('layouts.app')
 
 @section('content')
@@ -15,28 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                
-                <ul> 
-                    <li>
-                        <a href="/home">home</a>
-                    </li>
-                    <li>
-                        <a href="/profile">Profile</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                </ul>
-
                 <div class="card-header">{{ __('Halaman Home') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     {{ __('Anda Berhasil Masuk!') }}
                 </div>
             </div>
@@ -44,5 +19,3 @@
     </div>
 </div>
 @endsection
-</body>
-</html>
